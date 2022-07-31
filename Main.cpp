1d029c2bce6ac0e00c2bdfd80d134bac6b03556f
orion2124 <orion121242@gmail.com>
2022-07-31 16:17:35 +0800

    add masterBranchFuc

--- Main.cpp
+++ Main.cpp
@@ -10,10 +10,16 @@ void branchFuc()
     std::cout << __PRETTY_FUNCTION__ << std::endl;
 }
 
+void masterBranchFuc()
+{
+    std::cout << __PRETTY_FUNCTION__ << std::endl;
+}
+
 int main()
 {
     test();
     branchFuc();
+    masterBranchFuc();
     std::cout << "hello world" << std::endl;
 
     return 0;
