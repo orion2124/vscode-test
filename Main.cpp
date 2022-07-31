3b160a29d9e7bee7e6c6e38b0631cc67b7ff6ce1
orion2124 <orion121242@gmail.com>
2022-07-31 16:11:24 +0800

    add branch function

--- Main.cpp
+++ Main.cpp
@@ -5,9 +5,15 @@ void test()
     std::cout << __PRETTY_FUNCTION__ << std::endl;
 }
 
+void branchFuc()
+{
+    std::cout << __PRETTY_FUNCTION__ << std::endl;
+}
+
 int main()
 {
     test();
+    branchFuc();
     std::cout << "hello world" << std::endl;
 
     return 0;
